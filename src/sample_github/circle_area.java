e08f7ea997263cb247f859e54371c2e5a35625ab
praja <praja@KulkarniDesktop>
2022-08-13 11:22:12 -0700

    Upload my first project

---
+++ src/sample_github/circle_area.java
@@ -0,0 +1,17 @@
+package sample_github;
+
+public class circle_area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		area_circle(12);
+
+	}
+	public static void area_circle(double radius) {
+		double pi = 3.1415;
+		double area = pi * radius * radius;
+		System.out.println(area);
+		
+	}
+
+}
